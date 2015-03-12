7c565b63f1f4884fa88c336fe8d9b0418169c95e
ehud <ehud.kaldor@gmail.com>
2015-03-12 11:36:38 -0700

    added Create, Delete and Rename classes

--- src/main/scala/org/krazykat/iptables/dsl/States.scala
+++ src/main/scala/org/krazykat/iptables/dsl/States.scala
@@ -9,3 +9,18 @@ case class New() extends _State(" NEW ")
 case class Established() extends _State(" ESTABLISHED ")
 case class Related() extends _State(" RELATED ")
 case class Invalid() extends _State(" INVALID ")
+
+case class CreateChain(table: _Table, chainName: String)
+object CreateChain {
+  def apply(chainName: String): CreateChain = new CreateChain(Filter(), chainName)
+}
+
+case class DeleteChain(table: _Table, chain: _Chain)
+object DeleteChain {
+  def apply(chain: _Chain): DeleteChain = new DeleteChain(Filter(), chain)
+}
+
+case class RenameChain(table: _Table, chain: _Chain, newName: String)
+object RenameChain {
+  def apply(chain: _Chain, newName: String): RenameChain = new RenameChain(Filter(), chain, newName)
+}
