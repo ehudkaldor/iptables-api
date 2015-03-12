c575b9a2f4387de490fb8ba067a7576ec9c21e53
ehud <ehud.kaldor@gmail.com>
2015-03-12 13:22:43 -0700

    added logic to eliminate repetitive values in State

--- src/main/scala/org/krazykat/iptables/dsl/States.scala
+++ src/main/scala/org/krazykat/iptables/dsl/States.scala
@@ -2,7 +2,8 @@ package org.krazykat.iptables.dsl
 
 abstract class _State(val name: String)
 case class State(state: _State, states: _State*) {
-  val string = " " + (state +: states).map(_.name.trim).mkString(",") + " "
+  val set = (state +: states).toSet
+  val string = " --state " + set.map(_.name.trim).mkString(",") + " "
   override def toString = string  
 }
 case class New() extends _State(" NEW ")
