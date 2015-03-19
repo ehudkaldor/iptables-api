30bba1e82b6c3db2e9a508def9c81475e322f771
ehud <ehud.kaldor@gmail.com>
2015-03-18 23:15:21 -0700

    fixed ctstate typo removed spaces (will be added by implicits)

--- src/main/scala/org/krazykat/iptables/dsl/States.scala
+++ src/main/scala/org/krazykat/iptables/dsl/States.scala
@@ -8,7 +8,7 @@ case class States(state: _State, states: _State*) {
 
 case class CTStates(ctState: _CTState, ctStates: _CTState*){
   val set = (ctState +: ctStates).toSet
-  val string = " --state " + set.map(_.name.trim).mkString(",") + " "
+  val string = " --ctstate " + set.map(_.name.trim).mkString(",") + " "
   override def toString = string  
 }
 
@@ -17,7 +17,7 @@ abstract class _State(name: String) extends _CTState(name)
 
 abstract class _CTState(val name: String)
 
-case class New() extends _State(" NEW ")
-case class Established() extends _State(" ESTABLISHED ")
-case class Related() extends _State(" RELATED ")
-case class Invalid() extends _State(" INVALID ")
+case class New() extends _State("NEW")
+case class Established() extends _State("ESTABLISHED")
+case class Related() extends _State("RELATED")
+case class Invalid() extends _State("INVALID")
