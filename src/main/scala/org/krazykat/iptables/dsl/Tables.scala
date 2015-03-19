e5b8011d9a36ea52c375acf4b79bbc73aa6af7c4
ehud <ehud.kaldor@gmail.com>
2015-03-18 23:15:48 -0700

    removed spaces (will be added by implicits)

--- src/main/scala/org/krazykat/iptables/dsl/Tables.scala
+++ src/main/scala/org/krazykat/iptables/dsl/Tables.scala
@@ -1,8 +1,8 @@
 package org.krazykat.iptables.dsl
 
 abstract class _Table(table: String) { override def toString = table}
-case class Filter() extends _Table(" filter ")
-case class Nat() extends _Table(" nat ")
-case class Mangle() extends _Table(" mangle ")
-case class Raw() extends _Table(" raw ")
-case class Security() extends _Table(" security ")
+case class Filter() extends _Table("filter")
+case class Nat() extends _Table("nat")
+case class Mangle() extends _Table("mangle")
+case class Raw() extends _Table("raw")
+case class Security() extends _Table("security")
