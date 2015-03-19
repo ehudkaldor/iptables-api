f7717b1149f4ebc5fd7b4fb054003c8e943c869f
ehud <ehud.kaldor@gmail.com>
2015-03-18 23:12:06 -0700

    changed tableOpt: Option[_Table] to plain _Table

--- src/main/scala/org/krazykat/iptables/dsl/Chains.scala
+++ src/main/scala/org/krazykat/iptables/dsl/Chains.scala
@@ -1,27 +1,27 @@
 package org.krazykat.iptables.dsl
 
-abstract class _Chain(val name: String){ override def toString = name}
+abstract class _Chain(val name: String)
 sealed abstract class _BuiltInChain(name: String) extends _Chain(name)
-case class Input() extends _BuiltInChain(" INPUT ")
-case class Forward() extends _BuiltInChain(" FORWARD ")
-case class Output() extends _BuiltInChain(" OUTPUT ")
+case class Input() extends _BuiltInChain("INPUT")
+case class Forward() extends _BuiltInChain("FORWARD")
+case class Output() extends _BuiltInChain("OUTPUT")
 
-case class CreateChain(table: _Table, chainName: String)
-object CreateChain {
-  def apply(chainName: String): CreateChain = new CreateChain(Filter(), chainName)
-}
+case class NewChain(tableOpt: Option[_Table], chainName: String)
+//object NewChain {
+//  def apply(chainName: String): NewChain = new NewChain(Filter(), chainName)
+//}
 
-case class DeleteChain(table: _Table, chain: _Chain)
-object DeleteChain {
-  def apply(chain: _Chain): DeleteChain = new DeleteChain(Filter(), chain)
-}
+case class DeleteChain(tableOpt: Option[_Table], chain: _Chain)
+//object DeleteChain {
+//  def apply(chain: _Chain): DeleteChain = new DeleteChain(Filter(), chain)
+//}
 
-case class RenameChain(table: _Table, chain: _Chain, newName: String)
-object RenameChain {
-  def apply(chain: _Chain, newName: String): RenameChain = new RenameChain(Filter(), chain, newName)
-}
+case class RenameChain(tableOpt: Option[_Table], chain: _Chain, newName: String)
+//object RenameChain {
+//  def apply(chain: _Chain, newName: String): RenameChain = new RenameChain(Filter(), chain, newName)
+//}
 
-case class SetPolicy(table: _Table, chain: _BuiltInChain)
+case class SetPolicy(table: _Table, chain: _BuiltInChain, policy: _Policy)
 object SetPolicy {
-  def apply(chain: _BuiltInChain): SetPolicy = new SetPolicy(Filter(), chain)
+  def apply(chain: _BuiltInChain, policy: _Policy): SetPolicy = new SetPolicy(Filter(), chain, policy)  
 }
